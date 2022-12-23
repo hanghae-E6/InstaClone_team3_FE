c9bb23feec26a209e2aaa13c2660d88848a3e1af
heayounchoi <heayounchoi@gmail.com>
2022-12-23 23:17:17 +0900

    [view] 회원가입 view 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Demo from "./routes/Demo";
 import Home from "./routes/Home";
+import Signup from "./routes/Signup";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route exact path="/demo" element={<Demo />} />
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/signup" element={<Signup />} />
       </Routes>
     </Router>
   );
