2aa326077f461c8df6531b0453e2a24b05e51bd3
Jeonyujin77 <wjsdbwls7777@gmail.com>
2022-12-28 13:22:00 +0900

    #42 페이지별 마이페이지 관련 기능 추가-마이페이지 사용자 조회 추가

--- src/routes/Mypage.jsx
+++ src/routes/Mypage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import ProfileTemplate from "../components/layout/ProfileTemplate";
+import useSetUser from "../hooks/useSetUser";
 
 const Mypage = () => {
+  const param = useParams();
+  const user = useSetUser(param.userId);
+  console.log(user);
   return <ProfileTemplate>Mypage</ProfileTemplate>;
 };
 
